1be2f69d80abd1e21cec66d8760ee0e455ddbc5b
sezginoztemir <sezginoztemir@hotmail.com>
2023-03-14 19:59:38 +0300

    ...

---
+++ BlazorDemo/Shared/PostsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BlazorDemo.Shared
+{
+	public class PostsModel
+	{
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
+
---
+++ BlazorDemo/Shared/TodoModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BlazorDemo.Shared
+{
+    public class TodoModel
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
+
